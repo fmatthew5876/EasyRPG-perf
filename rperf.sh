885bae4248e5c41c3176f993bc8095bbb717b20d
Matthew Fioravante <fmatthew5876@gmail.com>
2019-09-17 20:51:50 -0400

    Add some lcf profiles

--- rperf.sh
+++ rperf.sh
@@ -2,10 +2,5 @@
 
 set -xe
 
-pid=$(pgrep easyrpg-player)
-if [ -z "$pid" ]; then
-	echo "No process id"
-	exit 1
-fi
-sudo perf record --call-graph dwarf -p ${pid}
+sudo perf record --call-graph dwarf "$@"
 
